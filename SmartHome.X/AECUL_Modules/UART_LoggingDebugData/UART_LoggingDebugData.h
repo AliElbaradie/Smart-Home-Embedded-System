3f0eda150a279cd5cdb9f988ef63bb98c8ffa71c
AliElbaradie <80052975+AliElbaradie@users.noreply.github.com>
2026-08-05 23:02:48 +0200

    Add files via upload

---
+++ SmartHome.X/AECUL_Modules/UART_LoggingDebugData/UART_LoggingDebugData.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   UART_LoggingDebugData.h
+ * Author: aliel
+ *
+ * Created on September 22, 2024, 5:44 PM
+ */
+
+#ifndef UART_LOGGINGDEBUGDATA_H
+#define	UART_LOGGINGDEBUGDATA_H
+
+#include "../../mcc_generated_files/eusart.h"
+
+void UART_LoggingDebugData_Send_String(uint8_t *String, uint16_t String_Length);
+
+
+#endif	/* UART_LOGGINGDEBUGDATA_H */
+
